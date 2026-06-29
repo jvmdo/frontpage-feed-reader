6bacb30d9b59220a21e4bf4f5fce967bf4da623f
João Oliveira <deoliveira.jvm@gmail.com>
2026-06-29 02:36:22 -0300

    chore: fix playwright webserver issue on wsl

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3321",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -76,8 +76,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: [
     {
-      command: "bun run start",
-      url: "http://localhost:3000",
+      command: "bun run start -p 3321",
+      url: process.env.CI ? "http://localhost:3321" : undefined,
       reuseExistingServer: !process.env.CI,
       env: {
         PLAYWRIGHT_TEST: "true",
@@ -85,7 +85,7 @@ export default defineConfig({
     },
     {
       command: "bun run serve -p 3432 ./e2e/fixtures",
-      url: "http://localhost:3432",
+      url: process.env.CI ? "http://localhost:3432" : undefined,
       reuseExistingServer: !process.env.CI,
     },
   ],
